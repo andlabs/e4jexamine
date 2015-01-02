956600392fde012dd090704e54baf4f117b003ee
Pietro Gagliardi <pietro10@mac.com>
2015-01-02 15:07:20 -0500

    Added the foundation for commands with arguments.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"io"
 	"flag"
+	"strings"
+	"strconv"
 )
 
 var r io.ReadSeeker
@@ -20,12 +22,33 @@ func init() {
 	flag.IntVar(&blocksize, "bs", 4096, "block size (bytes)")
 }
 
+func badline(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	fmt.Fprintf(os.Stderr, "\n")
+	flag.Usage()
+	os.Exit(1)
+}
+
+func getpos(command string) int64 {
+	m := strings.Split(command, ".")
+	if len(m) != 2 {
+		badline("invalid command specification: %s", command)
+	}
+	n, err := strconv.ParseInt(m[1], 0, 64)
+	if err != nil {
+		badline("error parsing command number %q: %v", m[1], err)
+	}
+	return n
+}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: %s [options] command [file]\n", os.Args[0])
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "commands:\n")
 		fmt.Fprintf(os.Stderr, "  summary - print a summary of all journal blocks\n")
+		fmt.Fprintf(os.Stderr, "  descdump.nnn - print a summary of the descriptor block at nnn and respective data blocks\n")
+		fmt.Fprintf(os.Stderr, "all nnn values are BYTE OFFSETS and may be octal with a leading 0 or hexadecimal with a leading 0x or 0X; decimal otherwise\n")
 	}
 	flag.Parse()
 	if flag.NArg() != 1 && flag.NArg() != 2 {
@@ -46,9 +69,10 @@ func main() {
 	switch {
 	case command == "summary":
 		summary()
+	case strings.HasPrefix(command, "descdump."):
+		pos := getpos(command)
+		fmt.Println(pos)
 	default:
-		fmt.Fprintf(os.Stderr, "unrecognized command %q\n", command)
-		flag.Usage()
-		os.Exit(1)
+		badline("unrecognized command %q", command)
 	}
 }
